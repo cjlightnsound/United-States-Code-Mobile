ad3e07daab98fb0cf4a2c75be64706e429b161be
Robert McAtee <robert_mcatee@txwb.uscourts.gov>
2021-10-05 16:34:02 -0500

    Added simple template for importing xhtml

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,39 +1,44 @@
 import * as React from 'react';
-import { StyleSheet,useWindowDimensions, ScrollView } from 'react-native';
+import { StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-
 // import Data
 import data from '../lib/tocRules.js';
+
+import {html} from '../data/PRELIMusc01.html.js';
 
 export default function Home({ navigation }: RootTabScreenProps<'RulesHome'>) {
   const { width } = useWindowDimensions();
   console.log(data)
   console.log(navigation)
   return (
-
     <View style={styles.container}>
-       <ScrollView style={{flex:1}}>
-      <Text style={styles.usc_title_head}>TITLE 1&mdash;GENERAL PROVISIONS</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.usc_title_ital_spanner}>
-      This title was enacted by act July 30, 1947, ch. 388, §1, 61 Stat. 633
-    </Text>
-    <TouchableHighlight
-      style={styles.button}
-      onPress={() => {
-        // Navigate using the `navigation` prop that you received
-        navigation.navigate('Federal Sentencing Guidelines');
+      <ScrollView style={{ flex: 1 }}>
+        <Text style={styles.usc_title_head}>TITLE 1&mdash;GENERAL PROVISIONS</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Text style={styles.usc_title_ital_spanner}>
+          This title was enacted by act July 30, 1947, ch. 388, §1, 61 Stat. 633
+        </Text>
 
-      }}
-    ><Text>Sentencing Guidelines</Text></TouchableHighlight>
-   </ScrollView>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => {
+            // Navigate using the `navigation` prop that you received
+            navigation.navigate('Federal Sentencing Guidelines');
+
+          }}
+        >
+          <Text>Sentencing Guidelines</Text>
+        </TouchableHighlight>
+      </ScrollView>
     </View>
-   
+
   );
 }
 
@@ -68,13 +73,13 @@ const styles = StyleSheet.create({
   usc_title_ital_spanner: {
     textAlign: "center",
     fontStyle: "italic",
-marginBottom: 10
+    marginBottom: 10
   },
   titleLink: {
-  fontSize: 20,
-  textAlign: "center",
-  fontStyle: "italic",
+    fontSize: 20,
+    textAlign: "center",
+    fontStyle: "italic",
   }
-  
-  
+
+
 });
